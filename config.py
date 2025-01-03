53d6e7feab83375bf75b2eb2153303ddf70fdb1d
Louiz77 <luizgu009@gmail.com>
2025-01-03 11:47:04 -0300

    1.7 configs do getenv

--- config.py
+++ config.py
@@ -6,3 +6,8 @@ load_dotenv()
 class Config:
     GOOGLE_SHEETS_CREDENTIALS_FILE = os.getenv("GOOGLE_SHEETS_CREDENTIALS_FILE")
     GOOGLE_SHEET_ID = os.getenv("GOOGLE_SHEET_ID")
+    ACCESS_TOKEN_URL = os.getenv("ACCESS_TOKEN_URL")
+    CLIENT_ID = os.getenv("CLIENT_ID")
+    CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+    ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+    AUTHORIZATION_CODE = os.getenv("AUTHORIZATION_CODE")
